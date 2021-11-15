73166acc7c3b7ef7f01d13646fc35ba48b4edff1
Irina <irinagmarques@hotmail.com>
2021-11-15 21:03:13 +0100

    updatePost useMutation

--- src/container/PostDetail.jsx
+++ src/container/PostDetail.jsx
@@ -1,4 +1,5 @@
-import { useQuery } from "react-query";
+import { useState } from "react";
+import { useQuery, useMutation } from "react-query";
 import styles from "./Posts.module.scss";
 
 async function fetchComments(postId) {
@@ -16,19 +17,16 @@ async function deletePost(postId) {
   return response.json();
 }
 
-async function updatePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "PATCH", data: { title: "REACT QUERY!" } }
-  );
-  return response.json();
-}
-
 export function PostDetail({ post }) {
+  const [showForm, setShowForm] = useState(false);
+  const [updatedTitle, setUpdatedTitle] = useState("");
   const { data, isLoading, error, isError } = useQuery(
     ["comments", post.id],
     () => fetchComments(post.id)
   ); //passing array for the query key and treat as a dependency array --> when key changes, create a new query
+
+  const deleteMutation = useMutation(postId => deletePost(postId));
+  const updateMutation = useMutation(postId => updatePost(postId));
 
   if (isLoading) return <h3>Loading...</h3>;
 
@@ -40,14 +38,56 @@ export function PostDetail({ post }) {
       </>
     );
 
+  const updatePost = async (e, postId) => {
+    e.preventDefault();
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/postId/${postId}`,
+      { method: "PATCH", data: { title: updatedTitle } }
+    );
+    return response.json();
+  };
+
+  const grabValue = e => {
+    setUpdatedTitle({ ...updatedTitle, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className={styles.container}>
       <h3 style={{ color: "#1d8dbe", paddingBottom: "0.5rem" }}>
         {post.title}
       </h3>
       <div>
-        <button className={styles.btn}>Delete</button>
-        <button className={styles.btn}>Update title</button>
+        <button
+          className={styles.btn}
+          onClick={() => deleteMutation.mutate(post.id)}
+        >
+          Delete
+        </button>
+        {deleteMutation.isError && <p style={{ color: "red" }}>Error</p>}
+        {deleteMutation.isLoading && (
+          <p style={{ color: "purple" }}>Deleting</p>
+        )}
+        {deleteMutation.isSuccess && <p style={{ color: "green" }}>Deleted</p>}
+        <button className={styles.btn} onClick={() => setShowForm(!showForm)}>
+          Update title
+        </button>
+        {showForm && (
+          <>
+            <form onSubmit={() => updateMutation.mutate(post.id)}>
+              <input
+                type='text'
+                name='title'
+                id='title'
+                onChange={grabValue}
+                className={styles.pd}
+                placeholder='type title'
+              />
+              <button type='submit' className={styles.btn}>
+                Submit
+              </button>
+            </form>
+          </>
+        )}
       </div>
       <p>{post.body}</p>
       <h4>Comments</h4>
